ccd39fe7e7766c28f2069ee8f4a2e912f6c130b2
Sean Gray <seangray.dev@gmail.com>
2023-04-09 20:35:26 -0400

    validation for signup...in prog

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ const validationSchema = Yup.object().shape({
     .email('Invalid email address')
     .required('Email is required'),
   password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
+    // .min(8, 'Password must be at least 8 characters')
     .required("Can't be empty"),
   repeatPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
