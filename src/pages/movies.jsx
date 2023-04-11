13ff565b82d9031b4ee4e38847015835fa68b406
Sean Gray <seangray.dev@gmail.com>
2023-04-11 05:42:25 -0400

    adjust to design specs

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -15,12 +15,12 @@ const Movies = () => {
       <h2 className='text-white text-xl md:text-[32px] font-light mb-4 md:mb-6 xl:mb-8'>
         Movies
       </h2>
-      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-[30px] xl:gap-10'>
+      <ul className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-[30px] xl:gap-10'>
         {movies.map((item, index) => (
-          <div className='relative' key={index}>
+          <li className='relative' key={index}>
             <div className='relative'>
-              <div className='opacity-0 hover:opacity-100 absolute w-full h-full'>
-                <div className='absolute bg-white/25 rounded-[28.5px] py-2 pl-2 top-1/2 left-1/2 transform -translate-x-[55px] -translate-y-[25px]'>
+              <div className='opacity-0 hover:opacity-100 absolute w-full h-full transition-opacity'>
+                <div className='absolute bg-white/25 rounded-[28.5px] py-2 pl-2 top-1/2 left-1/2 transform -translate-x-[55px] -translate-y-[25px] hover:scale-105 transition-transform'>
                   <button className='text-white flex gap-[19px] items-center pr-6'>
                     <Image src={play}></Image>
                     Play
@@ -28,7 +28,7 @@ const Movies = () => {
                 </div>
               </div>
               <img
-                className='rounded-lg mb-2 md:hidden'
+                className='rounded-lg mb-2 md:hidden w-full'
                 src={`/${item.thumbnail.regular.small}`}
               />
               <img
@@ -39,8 +39,16 @@ const Movies = () => {
                 className='hidden rounded-lg mb-2 md:hidden xl:block'
                 src={`/${item.thumbnail.regular.large}`}
               />
-              <div className='absolute opacity-50 top-1 md:top-4 right-1 md:right-4 rotate-2 bg-black rounded-full p-2 hover:cursor-pointer hover:scale-105'>
-                <Image src={iconEmpty}></Image>
+              <div className='bookmark-wrapper absolute opacity-50 top-1 md:top-4 right-1 md:right-4 rotate-2 bg-black rounded-full p-2 hover:cursor-pointer hover:opacity-100 hover:bg-white hover:scale-105 transition-all'>
+                <svg width='12' height='14' xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    className='bookmark-path'
+                    d='m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z'
+                    stroke='#FFF'
+                    stroke-width='1.5'
+                    fill='none'
+                  />
+                </svg>
               </div>
             </div>
             <div className='flex flex-col'>
@@ -75,9 +83,9 @@ const Movies = () => {
               </div>
               <p className='text-white md:text-lg'>{item.title}</p>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 };
