41e53b99acdca4f1ed463139a1fd30e4571f1c63
Sean Gray <seangray.dev@gmail.com>
2023-04-13 14:15:58 -0400

    fix trending heading

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -10,7 +10,9 @@ const Trending = () => {
 
   return (
     <section className='mt-6'>
-      <h2 className='text-white text-xl font-light mb-4'>Trending</h2>
+      <h2 className='text-white text-xl md:text-[32px] font-light mt-6 xl:mt-[34px] mb-4 md:mb-6 xl:mb-8'>
+        Trending
+      </h2>
       <div className='flex overflow-x-scroll hide-scroll-bar snap-x snap-mandatory'>
         <div className='flex flex-nowrap'>
           <ul className='flex gap-4 md:gap-10 flex-nowrap'>
