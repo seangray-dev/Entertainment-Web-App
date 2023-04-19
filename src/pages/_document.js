fae3d84367536265fa032b93fdae24768496ee43
Sean Gray <seangray.dev@gmail.com>
2023-04-18 21:46:24 -0400

    use logo as favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/assets/logo.svg'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
